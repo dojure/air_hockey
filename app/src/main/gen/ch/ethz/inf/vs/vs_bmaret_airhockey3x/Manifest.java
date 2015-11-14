325bb638a665cc82734b195506a285f1a575fa89
Valentin Venzin <valentin.venzin@mac.com>
2015-11-14 19:53:07 +0100

    Added further files

---
+++ app/src/main/gen/ch/ethz/inf/vs/vs_bmaret_airhockey3x/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package ch.ethz.inf.vs.vs_bmaret_airhockey3x;
+
+/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
+public final class Manifest {
+}
