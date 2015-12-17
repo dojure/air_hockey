6ab03e69582bef0d067406dd0637616e63352d30
Philipp Rimle <primle@student.ethz.ch>
2015-12-17 16:25:24 +0100

    fixed setting stuff

--- android/src/ch/ethz/inf/vs/vs_bmaret_airhockey3x/android/SettingsActivity.java
+++ android/src/ch/ethz/inf/vs/vs_bmaret_airhockey3x/android/SettingsActivity.java
@@ -3,14 +3,21 @@ package ch.ethz.inf.vs.vs_bmaret_airhockey3x.android;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.inputmethodservice.KeyboardView;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import ch.ethz.inf.vs.vs_bmaret_airhockey3x.android.communication.BluetoothComm;
 
@@ -23,7 +30,7 @@ import ch.ethz.inf.vs.vs_bmaret_airhockey3x.android.communication.BluetoothComm;
  */
 
 
-public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
+public class SettingsActivity extends AppCompatActivity {
 
     final static String LOGTAG = "SettingsActivity";
 
@@ -31,8 +38,7 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
     private SharedPreferences.Editor editor;
     private BluetoothComm mBC;
 
-    private static final String DEFAULT_NAME = "";
-
+    private String DEFAULT_NAME;
 
 
     @Override
@@ -40,14 +46,34 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        Button btn_save = (Button)findViewById(R.id.btn_save);
-        btn_save.setOnClickListener(this);
+//        Button btn_save = (Button)findViewById(R.id.btn_save);
+//        btn_save.setOnClickListener(this);
+
+        EditText inputName = (EditText) findViewById(R.id.input_device_name);
+        TextView.OnEditorActionListener okListener = new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    Log.d(LOGTAG, "saved");
+                    String deviceName = ((EditText) v).getText().toString();
+                    editor.putString("DEVICE_NAME", deviceName);
+                    editor.apply();
+                    mBC.changeDeviceName(deviceName);
+
+                    Toast toast = Toast.makeText(getApplicationContext(), "SAVED", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                return false;
+            }
+        };
+
+        inputName.setOnEditorActionListener(okListener);
 
         mBC = BluetoothComm.getInstance();
         preferences = getSharedPreferences("Settings", MODE_PRIVATE);
         editor = preferences.edit();
 
-
+        DEFAULT_NAME = mBC.getDeviceName();
     }
 
     @Override
@@ -73,29 +99,11 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
     }
 
     @Override
-    public void onResume(){
+    public void onResume() {
         super.onResume();
 
+
         String device_name = preferences.getString("DEVICE_NAME", DEFAULT_NAME);
-        ((EditText)findViewById(R.id.input_device_name)).setText(device_name);
+        ((EditText) findViewById(R.id.input_device_name)).setText(device_name);
     }
-
-
-    public void onClick(View b)
-    {
-        switch (b.getId()) {
-            case R.id.btn_save:
-                Log.d(LOGTAG, "onClickSave");
-                EditText device_name = (EditText) findViewById(R.id.input_device_name);
-                editor.putString("DEVICE_NAME", device_name.getText().toString());
-                editor.apply();
-                mBC.changeDeviceName(device_name.getText().toString());
-                //TODO: toast to show success/error....
-                Intent backToMainIntent = new Intent(this, MainActivity.class);
-                this.startActivity(backToMainIntent);
-                Log.d(LOGTAG, "back to MainActivity");
-                break;
-        }
-    }
-
 }
